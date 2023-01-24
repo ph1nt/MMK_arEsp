d72c9e390b3dd08d787ebc370ce0b2e0a0cbc527
ph1nt <hp0@me.com>
2023-01-24 21:46:58 +0100

    added titleBar color

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "227"
+  #define BUILD_NUMBER "229"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.4.227 - 2023-01-19 12:55:21.357613"
+  #define VERSION "v0.4.229 - 2023-01-24 21:40:53.092576"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.4.227"
+  #define VERSION_SHORT "v0.4.229"
 #endif
